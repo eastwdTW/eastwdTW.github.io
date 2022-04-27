453d33893e5faa070e5fc9c2e648d9e60e57b4a6
Vincent <vincent71334@gmail.com>
2022-04-28 00:09:58 +0800

    first

---
+++ main.js
@@ -0,0 +1,20 @@
+setInterval(() => toggleSound(), 100);
+setInterval(() => updateclock(), 100);
+updateclock();
+
+function toggleSound() {
+	var music = document.getElementById("bgm");
+	music.play();
+}
+
+function updateclock() {
+	var date = new Date();
+	var formatted_date = (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + " " + date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0");
+	var time = document.getElementById("time");
+	time.innerHTML = formatted_date;
+}
+
+function hide() {
+	var front = document.getElementById("front");
+	console.log('test')
+}
