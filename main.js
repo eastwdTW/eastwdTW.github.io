0cfb23dc9a416b2a215bd6533d9f9419b07cec9e
賴宗佑 <91512398+eastwdTW@users.noreply.github.com>
2022-04-30 22:34:01 +0800

    fix

--- main.js
+++ main.js
@@ -18,4 +18,4 @@ function updateclock() {
 	var formatted_date = (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + " " + date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0");
 	var time = document.getElementById("time");
 	time.innerHTML = formatted_date;
-}
+} 
