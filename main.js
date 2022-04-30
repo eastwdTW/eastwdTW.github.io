178e4282b53a37737be54a964a327a33019eec7f
賴宗佑 <91512398+eastwdTW@users.noreply.github.com>
2022-04-30 22:15:35 +0800

    change config

---
+++ main.js
@@ -0,0 +1,21 @@
+setInterval(() => updateclock(), 100);
+updateclock();
+
+var vol = document.getElementById("bgm");
+vol.volume = 0.5;
+
+var interact = document.body;
+interact.addEventListener("mousedown", () => toggleSound());
+interact.addEventListener("keydown", () => toggleSound());
+
+function toggleSound() {
+	var music = document.getElementById("bgm");
+	music.play();
+}
+
+function updateclock() {
+	var date = new Date();
+	var formatted_date = (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + " " + date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0");
+	var time = document.getElementById("time");
+	time.innerHTML = formatted_date;
+}
