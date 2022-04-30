fd5bc08867d47549fc4b5d4b32fc1482300dada0
Vincent <vincent71334@gmail.com>
2022-04-30 16:43:14 +0800

    trim

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ setInterval(() => updateclock(), 100);
 updateclock();
 
 var vol = document.getElementById("bgm");
-vol.volume = 0.1;
+vol.volume = 0.5;
 
 var interact = document.body;
 interact.addEventListener("mousedown", () => toggleSound());
@@ -18,9 +18,4 @@ function updateclock() {
 	var formatted_date = (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + " " + date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0");
 	var time = document.getElementById("time");
 	time.innerHTML = formatted_date;
-}
-
-function hide() {
-	var front = document.getElementById("front");
-	console.log('test')
 }
